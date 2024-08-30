d5a135782787dc015dc7c260285620c18c7bd86a
Yash Mathur <yashmat05@gmail.com>
2024-08-29 19:13:56 -0700

    implementing SwiperJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 // src/App.tsx
 
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CelebritySwiper from './components/Celebrities/CelebritySwiper';
 import Catalog from './components/Catalog/Catalog';
+import Styling from './components/Styling/Styling';
 import './style.scss';
 
 const App: React.FC = () => {
+  const [likedStyles, setLikedStyles] = useState<string[]>([]);
+  const [item, setItem] = useState<any | null>(null);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CelebritySwiper />} />
-        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/" element={<CelebritySwiper likedStyles={likedStyles} setLikedStyles={setLikedStyles}/>} />
+        <Route path="/catalog" element={<Catalog likedStyles={likedStyles} setItem={setItem}/>} />
+        <Route path="/styling" element={<Styling item={item}/>} />
       </Routes>
     </Router>
   );
